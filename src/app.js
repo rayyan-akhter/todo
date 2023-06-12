d52c7bfc66eeaeb7d9badec59fe4819adcf02c64
Rayyan Akhter <rayyanakhter2003@gmail.com>
2023-06-12 19:18:49 +0530

    added new functions and styles

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import "./style.css";
+import deleteIcon from "./assets/delete.png";
 export const App = () => {
   return (
     <>
@@ -16,31 +18,46 @@ const TodoList = () => {
   };
 
   const addTodo = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
     setTodos([...todos, input]);
     setInput("");
   };
 
+  const handlekeyPress =(e)=>{
+    if(e.key==="Enter") {
+        addTodo();
+    }
+  };
 
+  const deleteTodo =(index)=>{
+    const updateTodo =todos.filter((_,item)=> item !== index);
+    setTodos(updateTodo);
+    
+  };
 
   return (
     <main>
-      <div>
-        <form onSubmit={addTodo}>
+      <div className="child">
+        
         <input
           type="text"
           value={input}
           onChange={handleInputChange}
-
+          onKeyDown={handlekeyPress}
+          className="inputFeild"
           placeholder="Text here"
           />
-        <button >+</button>
-          </form>
+        <button onClick={addTodo} className="addBtn" >+</button>
+          
+        
       </div>
-      <div>
-        <ul>
+      <div className="secondChild">
+        <ul className="todoList">
           {todos.map((todo, index) => (
-            <li key={index}>{todo}</li>
+            <li className="todo" key={index}>{todo}
+            <img src={deleteIcon} alt="delete icon" className="dltIcon" onClick={() => deleteTodo(index)}/>
+            </li>
+    
           ))}
         </ul>
       </div>
